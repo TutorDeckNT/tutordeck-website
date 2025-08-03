b3156e5d1c46a8dac8566e4e8afb569643e5fbd4
TutorDeckNT <prospertutorcorp@gmail.com>
2025-08-03 17:45:53 -0500

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { HashRouter } from 'react-router-dom'
+
+// Use HashRouter for GitHub Pages compatibility
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+)
