c9723c51a8a100eb90ee7f22c12b1984d08c0059
TutorDeckNT <prospertutorcorp@gmail.com>
2025-08-03 17:46:15 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,34 @@
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ChaptersPage from './pages/ChaptersPage';
+import GetInvolvedPage from './pages/GetInvolvedPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
+function App() {
+  return (
+    <>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/chapters" element={<ChaptersPage />} />
+        <Route path="/get-involved" element={<GetInvolvedPage />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
