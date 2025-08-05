86db1c2c7ae5f8361a69594df0875e221cce4403
TutorDeckNT <prospertutorcorp@gmail.com>
2025-08-05 12:35:10 -0500

    Create ProtectedRoute.tsx

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+const ProtectedRoute = () => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    // You can add a loading spinner here
+    return <div className="h-screen w-full flex items-center justify-center text-primary">Loading...</div>;
+  }
+
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
