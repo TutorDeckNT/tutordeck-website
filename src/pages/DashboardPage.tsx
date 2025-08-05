b6e2c99a4205880e1880d679cc8a8e3ae5c9a77f
TutorDeckNT <prospertutorcorp@gmail.com>
2025-08-05 12:40:46 -0500

    Update DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -41,7 +41,7 @@ const DashboardPage = () => {
                         );
                         setActivities(userActivities);
                     },
-                    error: (err: Papa.ParseError) => { // <-- THIS IS THE FIX
+                    error: (err: Error) => { // <-- THIS IS THE FIX
                         throw new Error(`Parsing error: ${err.message}`);
                     }
                 });
