4066c1da982929c46354d3d32fd21e97cc29a148
TutorDeckNT <prospertutorcorp@gmail.com>
2025-09-13 20:53:25 -0500

    Update LogActivityModal.tsx

--- src/components/LogActivityModal.tsx
+++ src/components/LogActivityModal.tsx
@@ -27,7 +27,6 @@ const LogActivityModal = ({ isOpen, onClose, onActivityAdded }: LogActivityModal
     // This effect initializes and destroys the datepicker instance
     useEffect(() => {
         if (isOpen && datepickerInputRef.current) {
-            // THE TYPO IS FIXED HERE
             const datepicker = new Datepicker(datepickerInputRef.current, {
                 autohide: true,
                 format: 'yyyy-mm-dd',
